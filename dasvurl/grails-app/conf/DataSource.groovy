c8e88b5e8e8c2e201b0a442187c10e8543b4f169
dhanendra <dhanendra.singh@appdirect.com>
2016-09-24 20:33:09 +0530

    database changed

--- dasvurl/grails-app/conf/DataSource.groovy
+++ dasvurl/grails-app/conf/DataSource.groovy
@@ -31,7 +31,10 @@ environments {
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            driverClassName = "com.mysql.jdbc.Driver"
+            url = "jdbc:mysql://localhost/dasvurl?zeroDateTimeBehavior=convertToNull&useUnicode=true&characterEncoding=UTF-8&characterSetResults=UTF-8&autoReconnect=true"
+            username = "root"
+            password = "root"
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
