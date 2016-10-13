1059de85e636c8d2d4f01430c60dac5875ee542a
dhanendra <dhanendra.singh@appdirect.com>
2016-10-13 08:14:35 +0530

    fixed bootstrap

--- dasvurl/grails-app/conf/BootStrap.groovy
+++ dasvurl/grails-app/conf/BootStrap.groovy
@@ -5,9 +5,15 @@ import com.dasvurl.UserRole
 class BootStrap {
 
     def init = { servletContext ->
-        User user  = new User('rohit','password1').save()
-        Role role_user = new Role('USER').save()
-        new UserRole(user , role_user).save(failOnError: true)
+        User user = User.findByUsername('rohit');
+        Role role_user = Role.findByAuthority("USER")
+        if(!user) {
+            user = new User('rohit','password1').save()
+        }
+        if(!role_user) {
+            role_user = new Role('USER').save()
+            new UserRole(user , role_user).save(failOnError: true)
+        }
     }
     def destroy = {
     }
